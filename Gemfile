0fade3b580c18185381cd91efb6057eafc393d77
Marek Nowak <mareknowakk@gmail.com>
2012-05-17 20:55:07 +0200

    backbone

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :test do
   gem 'factory_girl'
 end
 
+gem 'backbone-rails'
 gem 'jquery-rails'
 gem 'twitter-bootstrap-rails'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
 //= require twitter/bootstrap
 //= require_tree .
