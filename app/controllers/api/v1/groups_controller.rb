a8d40b4bba4f018a80b5399c285f880086b42860
Michał Krajewski <mkrajewski90@gmail.com>
2012-05-17 20:17:39 +0200

    delete group

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::GroupsController < ApplicationController
   end
   
   def destroy
-    Group.find(:id).destroy
+    Group.find(params[:id]).destroy
+    redirect_to api_v1_groups_path
   end
 end
--- spec/controllers/api/v1/groups/destroy_spec.rb
+++ spec/controllers/api/v1/groups/destroy_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Api::V1::GroupsController do
+  let!(:group) { FactoryGirl.create :group, :name => "My name" }
+  
   before(:each) do 
-    group = create(:group)
     delete :destroy, :id => group.id, :format => :json
   end 
   
