938077bbf3f254638eb5662519edfdac52f4844e
Michal Bugno <michal.bugno@gmail.com>
2012-05-17 20:11:49 +0200

    Groups#index returns name in response

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120517172849) do
+ActiveRecord::Schema.define(:version => 20120517173215) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "people", :force => true do |t|
     t.string   "email"
--- spec/controllers/api/v1/groups/index_spec.rb
+++ spec/controllers/api/v1/groups/index_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Api::V1::GroupsController do
+  let!(:group) { FactoryGirl.create :group, :name => "My name" }
   before { get :index, :format => :json }
+
   it { should respond_with(:success) }
+
+  it "includes name in response" do
+    groups = Yajl::Parser.parse(response.body)
+    groups.size.should == 1
+    groups[0]['name'].should == 'My name'
+  end
 end
