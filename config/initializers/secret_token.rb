4dd90fbe2bb0853182501e7e0576563fed268de9
Michal Bugno <michal.bugno@gmail.com>
2012-05-17 18:55:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walleet::Application.config.secret_token = '473ce5336388b111ca947c0cc9bf0d37a3bccc8f3ba4eec7d89fb9173bba421a0c0b4c5667552d10a014269f952be024d027747834f1dbb5551ef3a652648d45'
