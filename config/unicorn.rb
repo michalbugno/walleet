e3462b3bd00523cd7076e18ba8171955b1b1d8f8
Michal Bugno <michal.bugno@gmail.com>
2012-08-12 22:05:44 +0200

    unicorn config fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,10 @@ app_env = ENV["APP_ENV"] || "development"
 root_dir = File.expand_path(File.join(__FILE__, "..", ".."))
 
 pid File.join(root_dir, "tmp", "pids", app_env + ".pid")
+
+listen File.join(root_dir, "tmp", "unicorn.sock")
+
 logger Logger.new(File.join(root_dir, "log", app_env + ".log"))
+
 working_directory root_dir
-worker_processes 4
+worker_processes 3
